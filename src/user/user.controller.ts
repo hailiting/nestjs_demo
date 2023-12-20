50c54e709f9a176e45963e22990edc6e26d6fa8a
HaiSli <admin@192.168.0.14>
2023-12-20 15:02:00 +0800

    add

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
@@ -7,8 +7,8 @@ export class UserController {
     // this.userService = new UserService()
   }
   @Get()
-  getUsers(): any {
-    return this.userService.getUsers();
+  getUsers(@Query('len') len: string): any {
+    return this.userService.getUsers(len);
   }
   @Post()
   addUsers(): any {
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,9 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UserService {
-  getUsers(): any {
+  getUsers(len: string): any {
+    console.log(Number(len), len);
+    if (isNaN(Number(len))) {
+      return {
+        code: 0,
+        msg: 'error',
+      };
+    }
     return {
       code: 'mmm',
+      data: Array.from({ length: Number(len) }, (_, index) => `${index + 1}`),
     };
   }
   addUsers(): any {
